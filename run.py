7c82593140c6ba0776833ef3829659c0b8d46464
mhellnerdev@gmail.com <mhellnerdev@gmail.com>
2021-12-31 20:12:58 -0500

    comments in main app

--- run.py
+++ run.py
@@ -3,11 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# the string operator that is passed into the function decorator here will create a route to the flask "view"
 @app.route("/")
 def index():
     return '<h1>Interior Crocodile Alligator</h1>'
 
 
+# the string operator that is passed into the function decorator here will create a route to the flask "view"
 @app.route('/information')
 def info():
     return "<h1>I Drive a Chevy Movie Theater</h1>"
@@ -16,4 +18,4 @@ def info():
 if __name__ == '__main__':
     app.run(debug=True)
 else:
-    print('fail')
+    print('There was an error launching run.py')
